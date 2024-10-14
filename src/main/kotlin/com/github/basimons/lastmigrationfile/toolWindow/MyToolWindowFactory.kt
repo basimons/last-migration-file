33a6c001d3254a5ec246a286bd1b2f18d6a83627
GitHub Action <action@github.com>
2024-10-14 14:49:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/basimons/lastmigrationfile/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/basimons/lastmigrationfile/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.basimons.lastmigrationfile.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.basimons.lastmigrationfile.MyBundle
+import com.github.basimons.lastmigrationfile.services.MyProjectService
 import javax.swing.JButton
 
 
